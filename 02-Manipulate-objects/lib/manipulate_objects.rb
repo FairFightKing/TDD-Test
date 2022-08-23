5796ffb5e4cb3a17dc3144723f212da611d1bbd0
FairFightKing <sosthenefruchard@gmail.com>
2022-08-23 15:54:03 +0200

    manipulate object

--- 02-Manipulate-objects/lib/manipulate_objects.rb
+++ 02-Manipulate-objects/lib/manipulate_objects.rb
@@ -1,1 +1,14 @@
 # BONUS ONLY: Write the array_to_hash and palindrome? methods
+
+def array_to_hash(array)
+    hash = {}
+    array2 = []
+    array.each_with_index do |value,index|
+       hash[index] = value
+       array2.push(index)
+    end
+    array2.each do |value|
+        hash[key + value] = hash.delete(value)
+    end
+    return hash
+end
--- 02-Manipulate-objects/spec/manipulate_objects_spec.rb
+++ 02-Manipulate-objects/spec/manipulate_objects_spec.rb
@@ -1,3 +1,18 @@
 # TO DO: Write the test that ensure:
 # - Our array_to_hash method successfully converts an array to a hash, with the given pattern
 # - Our palindrome? method successfully returns whether the given argument is a plindrome or not
+
+require './lib/manipulate_objects'
+
+arg = ['TDD','with','Ruby']
+describe '#manipulate_objects' do
+    it 'arg should be an array' do
+        expect(arg).to be_a_kind_of(Array)
+    end
+    it 'output should be an hash' do
+        expect(array_to_hash(arg)).to be_a_kind_of(Hash)
+    end
+    it 'should transform arg into an hash' do
+        expect(array_to_hash(arg)).to eq({key0: 'TDD',key1: 'with',key2:'Ruby'})
+    end
+end
